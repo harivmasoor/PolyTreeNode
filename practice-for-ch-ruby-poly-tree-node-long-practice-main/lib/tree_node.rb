35060132dfc67d0cba1d16b33d51f2a306dac294
stefanlazarevic <stefanlaza97@gmail.com>
2023-06-09 13:42:25 -0700

    Add PolyTreeNode class

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,43 @@
 class PolyTreeNode
-  
+    def initialize(value, children = [])
+        @value = value
+        @children = []
+        @parent = nil
+    end
+
+    attr_reader :value, :children, :parent
+
+    def parent=(node)
+        @parent.children.delete(self) unless @parent.nil?
+        @parent = node 
+        @parent.children << self unless @parent.nil?
+    end
+
+    def add_child(child_node)
+        child_node.parent = self    
+    end
+
+    def remove_child(child_node)
+        if self.children.include?(child_node)
+            child_node.parent = nil
+        else
+            raise
+        end
+    end
+
+    def dfs(target)
+        
+    end
+
+    def bfs(target)
+
+    end
 end
+
+d = PolyTreeNode.new('d')
+e = PolyTreeNode.new('e')
+f = PolyTreeNode.new('f')
+g = PolyTreeNode.new('g')
+b = PolyTreeNode.new('b', [d,e])
+c = PolyTreeNode.new('c', [f,g])
+a = PolyTreeNode.new('a', [b,c])
